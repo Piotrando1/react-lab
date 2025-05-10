7049b57ffafedc6eb912241a473c8ff64ceaaa8d
Piotrando1 <czernik.piotr@gmail.com>
2025-05-10 15:48:09 +0200

    Czwarty Commit

--- src/App.js
+++ src/App.js
@@ -5,41 +5,61 @@ import {useState} from "react";
 function App() {
 
    // let email = 'fracz@agh.edu.pl';
-    const [email, setEmail] = useState('fracz@agh.edu.pl')
-
+    const [email, setEmail] = useState('')
+    //const [isLoggedIn, setIsLoggedIn] = useState(false)
+    const [loggedInAs, setLoggedInAs] = useState(null)
 
     function handleChange(event)
     {
         setEmail(event.target.value);
     }
 
+    function handleAuthenticate(){
+    setLoggedInAs(email)
+    setEmail('')
+}
 
-    let message = '';
-    if (email.length < 10) {
-        message = 'Ale masz krótki adres!';
-    } else if (email.length < 15) {
-        message = 'Twój adres e-mail jest w sam raz.';
-    } else {
-        message = 'Twój adres e-mail jest stanowczo za długi.';
+    let content;
+
+    if(loggedInAs) {
+        content = <div>
+            <h2> Witaj {loggedInAs} !</h2>
+                <a onClick ={()=> setLoggedInAs(null)}> Wyloguj </a>
+        </div>
+
+    }else{
+        content = <div>
+            <label>
+                Zaloguj się e-mailem
+                <input
+                    type="text"
+                    value={email}
+                    onChange={handleChange}
+                    placeholder="Twój e-mail"
+                />
+            </label>
+            <button onClick={handleAuthenticate}>Wchodzę</button>
+
+        </div>
     }
+
+    // //let message = '';
+    // if (email.length < 10) {
+    //     message = 'Ale masz krótki adres!';
+    // } else if (email.length < 15) {
+    //     message = 'Twój adres e-mail jest w sam raz.';
+    // } else {
+    //     message = 'Twój adres e-mail jest stanowczo za długi.';
+    // }
 
 
     return (
-        <div>
+        <div className="container">
+            <h1>Witaj w systemie do zapisów na zajęcia</h1>
 
-            <h1>System do zapisów na zajęcia</h1>
-            <h2>Twój e-mail to {email}</h2>
 
-            <input
-                type="text" value={email} onChange={handleChange}
-                placeholder="Wpisz swój e-mail"
-            />
+            {content}
 
-            <button type="button" onClick={() => alert(email)}>
-                Wyświetl mój e-mail w alercie
-            </button>
-
-            <p> {message} </p>
         </div>
 
     );
